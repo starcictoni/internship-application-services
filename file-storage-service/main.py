5eb91e7586cef027d1965ceadb2b81c494d07b18
Nikola Tanković <nikola.tankovic@gmail.com>
2021-09-17 15:22:00 +0200

    Add unicorn

--- file-storage-service/main.py
+++ file-storage-service/main.py
@@ -6,7 +6,10 @@ import os
 if not os.path.isdir("public"):
     os.mkdir("public")
 
-#Handler for bytes file, eg. direct upload from attachment control in form
+routes = web.RouteTableDef()
+
+# Handler for bytes file, eg. direct upload from attachment control in form
+@routes.post("/post/file")
 async def handle_post_file(request):
     reader = aiohttp.MultipartReader.from_response(request)
 
@@ -20,27 +23,43 @@ async def handle_post_file(request):
         if ".pdf" in part.filename:
             filename = part.filename
             metadata = await part.read()
-            new_pdf = open("public/"+filename, "wb")
+            new_pdf = open("public/" + filename, "wb")
             new_pdf.write(metadata)
             print(f"Received {filename}, created new file at public/{filename}")
-    
-    return web.json_response({"status":"ok"})
+
+    return web.json_response({"status": "ok"})
 
 
-app = web.Application()
-app.add_routes([web.post("/post/file", handle_post_file)])
-app.add_routes([web.static("/public", "./public")])
+app = None
 
-cors = aiohttp_cors.setup(app, defaults={
-    "*": aiohttp_cors.ResourceOptions(
-        allow_credentials=True,
-        expose_headers="*",
-        allow_headers="*",
-        allow_methods="*"
+
+def run():
+    global app
+
+    app = web.Application()
+    app.add_routes(routes)
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+                allow_methods="*",
+            )
+        },
     )
-})
 
-for route in list(app.router.routes()):
-    cors.add(route)
+    for route in list(app.router.routes()):
+        cors.add(route)
 
-web.run_app(app, port=8084)
+    return app
+
+
+async def serve():
+    return run()
+
+
+if __name__ == "__main__":
+    app = run()
+    web.run_app(app, port=8084)
